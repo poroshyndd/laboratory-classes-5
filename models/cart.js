02530c3f436db358ff670755588d51ea63a14597
poroshyndd <poroshyndd@gmail.com>
2025-04-30 20:36:41 +0200

    Update cart.js

--- models/cart.js
+++ models/cart.js
@@ -1,1 +1,45 @@
+const Product = require('./Product');
+
+class Cart {
+  static #items = [];
+
+  static add(productName, quantity = 1) {
+    const product = Product.findByName(productName);
+    if (!product) {
+      throw new Error(`Product with name \"${productName}\" not found.`);
+    }
+
+    const existingItem = this.#items.find(
+      (item) => item.product.name === productName
+    );
+
+    if (existingItem) {
+      existingItem.quantity += quantity;
+    } else {
+      this.#items.push({ product, quantity });
+    }
+  }
+
+  static getItems() {
+    return this.#items;
+  }
+
+  static getTotalPrice() {
+    return this.#items.reduce(
+      (total, item) => total + item.product.price * item.quantity,
+      0
+    );
+  }
+
+
+  static getProductsQuantity() {
+    return this.#items.reduce((sum, item) => sum + item.quantity, 0);
+  }
+
+  static clearCart() {
+    this.#items = [];
+  }
+}
+
+module.exports = Cart;
 
