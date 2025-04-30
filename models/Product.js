dc7a319b0efe4366ed1beda429fb214a780dd8bd
poroshyndd <poroshyndd@gmail.com>
2025-04-30 20:37:37 +0200

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,7 +1,8 @@
 class Product {
-  constructor(name, description) {
+  constructor(name, description, price) {
     this.name = name;
     this.description = description;
+    this.price = price;
   }
 
   static #products = [];
@@ -29,6 +30,10 @@ class Product {
 
     return this.#products[this.#products.length - 1];
   }
+}
+
+module.exports = Product;
+
 }
 
 module.exports = Product;
