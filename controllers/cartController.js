cbb4c1910ff27542dbeba0c66d11c8f19ec316fb
poroshyndd <poroshyndd@gmail.com>
2025-04-30 20:34:51 +0200

    Update cartController.js

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,1 +1,15 @@
+const Product = require("../models/Product");
+const Cart = require("../models/cart");
+const { STATUS_CODE } = require("../constants/statusCode");
 
+exports.addProductToCart = (request, response) => {
+  Product.add(request.body);
+  const { name, quantity = 1 } = request.body;
+  Cart.add(name, quantity);
+  response.status(STATUS_CODE.FOUND).redirect("/products/new");
+};
+
+exports.getProductsCount = (request, response) => {
+  const count = Cart.getProductsQuantity();
+  response.status(STATUS_CODE.OK).json({ count });
+};
