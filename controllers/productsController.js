6d6d37a92ea8c2b8c37b6cf47a6104098eee4f41
poroshyndd <poroshyndd@gmail.com>
2025-05-01 11:13:14 +0200

    Update productsController.js

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -23,6 +23,12 @@ exports.getAddProductView = (request, response) => {
   });
 };
 
+exports.addNewProduct = (request, response) => {
+  Product.add(request.body);
+
+  response.status(STATUS_CODE.FOUND).redirect("/products/new");
+};
+
 exports.getNewProductView = (request, response) => {
   const newestProduct = Product.getLast();
 
